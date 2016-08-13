bade5016df0672c8a769c962d05edd9b3d650317
Vishal Chovatiya <visheshchovatiya@gmail.com>
2016-08-13 18:50:36 +0530

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <algorithm>
-#include <list>
+#include <list>			
 using namespace std;
-
-void printList(list<int>	l){
-	for_each( l.begin(), l.end(), [] (const int integer) { cout << integer << " ";} );		// -std=c++11 Compiler Option
-	cout<<endl;
-}
 
 int main(){
 	int arr[] =  {2,3,4,5,6,7};
 	
 	std::list<int> DLL(arr, arr+6);					// Doubly Linked List
 	
+	/* Erasing Elements from list */
 	DLL.erase(DLL.begin());
-	DLL.erase(DLL.begin(), DLL.end());
-	
-	DLL.push_front(1);
-    DLL.push_back(3);
-    
-    cout<<DLL.front()<<endl;
-    cout<<DLL.back()<<endl;
-    
-    DLL.pop_front();
-    DLL.pop_back();
-	
+	DLL.erase(DLL.begin(), find( DLL.begin(), DLL.end(), 4));
 	DLL.remove(2);								// Remove all elements equal to 2
 	DLL.remove_if([](int n){ return n > 5; }); 	// Remove all elements greater than 5
-
-	// Insert an integer before 1 by searching
-    auto it = find(DLL.begin(), DLL.end(), 1);
-    if (it != DLL.end()) {
-        DLL.insert(it, 0);
-    }
 	
+	/* Manipulating individual Elements from List*/
+	DLL.push_front(1);
+	cout<<"DLL Front  Element = "<<DLL.front()<<endl;
+	DLL.pop_front();	
+	DLL.push_back(3);	
+	cout<<"DLL Second Element = "<<DLL.back()<<endl;	
+	DLL.pop_back();
+	
+	/* Insert an integer before 1 by searching */
+	auto it = find(DLL.begin(), DLL.end(), 1);
+	if (it != DLL.end()) {
+	    DLL.insert(it, 0);
+	}
+	
+	/* List Capacity Methods */
+	if( !DLL.empty() )
+		cout<<"DLL Size = "<<DLL.size()<<endl;	
+		
+	/* Printing List */	
 	for( auto i : DLL)
 		cout << i << " ";
-		
-	if( !DLL.empty() )
-		cout<<" DLL Size = "<<DLL.size()<<endl;	
+	cout<<endl;	
+	for_each( DLL.begin(), DLL.end(), [] (const int integer) { cout << integer << " ";} );		// -std=c++11 Compiler Option
+	
 	
 	return 0;
 }
