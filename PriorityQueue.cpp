64391779ead98f43f5c12b7887fbf89c0283a85b
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-06-07 15:12:47 +0530

    Update PriorityQueue.cpp

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,46 +1,70 @@
-#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <algorithm>
 #include <queue>
-
 using namespace std;
 
-/*
-	Question:- Min & Max heap using C++ STL
-	
-	template< class T, 
-			  class Container = std::vector<T>, 
-			  class Compare = std::less<typename Container::value_type>
-			
-	>    class priority_queue;
-	
-	Solution:- Use Priority Queue
-*/
 
-int main()
+template<
+		typename T,
+		typename Compare = std::less<T>
+		>
+class pq : public std::priority_queue<T, std::vector<T>, Compare>
 {
-	// Declaring Queue	
-	struct
-	{
-		bool operator()(pair<int,string> const &a, pair<int,string> const &b)
-		{
-			return a.first > b.first;	// This statement make Min & Max heap
+public:
+	void print(){
+		for(auto it=this->c.begin(); it!= this->c.end(); ++it){
+			cout<<*it<<" ";
 		}
-	}compare;
-	
-	priority_queue< pair<int,string>, vector<pair<int,string>>, decltype(compare)>  q;		// decltype extract the data type similar as sizeof in c
-	
-	// Initializing Queue	
-	q.push( make_pair( 1, "RAM" ));
-	q.push( make_pair( 5, "KAM" ));
-	q.push( make_pair( 3, "JAM" ));
-	q.push( make_pair( 2, "BAM" ));
-	q.push( make_pair( 4, "NAM" ));	
-	
-	// Printing
-	while( !q.empty())
-	{
-		cout<<q.top().second<<" ";
+		cout<<endl;
+	}
+
+	bool remove(const T& value) {
+		auto it = std::find(this->c.begin(), this->c.end(), value);
+		if (it != this->c.end()) {
+			this->c.erase(it);
+			std::make_heap(this->c.begin(), this->c.end(), this->comp);
+			return true;
+		}
+		return false;
+	}
+
+	bool change(const T& oldVal, const T& newVal){
+		auto it = std::find(this->c.begin(), this->c.end(), oldVal);
+		if (it != this->c.end()) {
+			this->c.erase(it);
+			this->c.push_back(newVal);
+			std::make_heap(this->c.begin(), this->c.end(), this->comp);
+			return true;
+		}
+		return false;
+	}
+};
+
+
+int main() {
+
+	pq<pair<int,string>> q;
+
+	q.push(make_pair(0, "0"));
+	q.push(make_pair(1, "1"));
+	q.push(make_pair(2, "2"));
+
+	q.change(make_pair(1,"1"), make_pair(1,"11"));
+
+	q.remove(make_pair(0,"0"));
+
+	// q.print();
+
+	while(!q.empty()){
+		cout<<q.top().first<<" "<<q.top().second<<endl;
+
 		q.pop();
 	}
- 
+
+
     return 0;
 }
