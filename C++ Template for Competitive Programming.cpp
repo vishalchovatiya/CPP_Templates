8ced5dcf8ad56cf08bd0b9d688bbba060c656a2e
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-06-13 20:27:00 +0530

    Update C++ Template for Competitive Programming.cpp

--- C++ Template for Competitive Programming.cpp
+++ C++ Template for Competitive Programming.cpp
@@ -14,10 +14,8 @@
 #include <algorithm>
 #include <functional>
 using namespace std; 
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
-#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+
+
 inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
 const int INF = 1<<29;
 typedef long long ll;
@@ -27,8 +25,15 @@ inline void set_bit(int & n, int b) { n |= two(b); }
 inline void unset_bit(int & n, int b) { n &= ~two(b); }
 inline int last_bit(int n) { return n & (-n); }
 inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
-template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
-template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+#define DEBUG(X)    std::cout<<#X<<" = "<<X<<std::endl;
+#define PRINT(C,WAY)    do
+                        {
+                            for(auto&& i : C) {
+                                WAY;
+                            }
+                            cout<<endl;
+                        } while (0);
+
 /////////////////////////////////////////////////////////////////////
 int main()
 {
